923234be2689f6821f55c3e2e9f9ebff54b72b63
Davin Reynolds <dmartelbusiness21@gmail.com>
2026-06-10 20:44:58 -0700

    fix: s3 remote backend, workflow step outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,12 @@ terraform {
     random  = { source = "hashicorp/random", version = "~> 3.6" }
     archive = { source = "hashicorp/archive", version = "~> 2.4" }
   }
+
+  backend "s3" {
+    bucket = "acme-health-intake-tfstate-316391d2"
+    key    = "capstone/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
